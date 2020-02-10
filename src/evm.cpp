59e3938a78c998092fc6850e2cc5a803ca12de18
Ricardo Toledo <rictoledo80@gmail.com>
2020-02-10 12:33:15 -0300

    Implements RIPEMD160

--- src/evm.cpp
+++ src/evm.cpp
@@ -524,6 +524,142 @@ static uint256_t sha256(const uint8_t *buffer, uint32_t size)
     return uint256_t::from(output);
 }
 
+static inline uint32_t b2w32le(const uint8_t *b)
+{
+    return 0
+        | ((uint64_t)b[3] << 24)
+        | ((uint64_t)b[2] << 16)
+        | ((uint64_t)b[1] << 8)
+        | (uint64_t)b[0];
+}
+
+static inline void w2b32le(uint32_t w, uint8_t *b)
+{
+    b[0] = (uint8_t)w;
+    b[1] = (uint8_t)(w >> 8);
+    b[2] = (uint8_t)(w >> 16);
+    b[3] = (uint8_t)(w >> 24);
+}
+
+static inline uint32_t rol(uint32_t x, int n) { return (x << n) ^ (x >> (32 - n)); }
+
+static inline uint32_t f(uint32_t x, uint32_t y, uint32_t z) { return x ^ y ^ z; }
+static inline uint32_t g(uint32_t x, uint32_t y, uint32_t z) { return (x & y) ^ (~x & z); }
+static inline uint32_t h(uint32_t x, uint32_t y, uint32_t z) { return (x | ~y) ^ z; }
+static inline uint32_t i(uint32_t x, uint32_t y, uint32_t z) { return (x & z) ^ (y & ~z); }
+static inline uint32_t j(uint32_t x, uint32_t y, uint32_t z) { return x ^ (y | ~z); }
+
+static void ripemd160(const uint8_t *message, uint32_t size, bool compressed, uint8_t *output)
+{
+    if (!compressed) {
+        uint64_t bitsize = 8 * size;
+        uint32_t modulo = (size + 1 + 8) % 64;
+        uint32_t padding = modulo > 0 ? 64 - modulo : 0;
+        uint32_t b_len = size + 1 + padding + 8;
+        uint8_t b[b_len];
+        for (uint32_t i = 0; i < size; i++) b[i] = message[i];
+        for (uint32_t i = size; i < b_len; i++) b[i] = 0;
+        b[size] = 0x80;
+        w2b32le(bitsize, &b[b_len-8]);
+        w2b32le(bitsize >> 32, &b[b_len-4]);
+        ripemd160(b, b_len, true, output);
+        return;
+    }
+    static uint32_t (*F1[5])(uint32_t,uint32_t,uint32_t) = { f, g, h, i, j };
+    static const uint32_t K1[5] = { 0x00000000, 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xa953fd4e };
+    static const uint8_t O1[5][16] = {
+        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
+        { 7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8 },
+        { 3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12 },
+        { 1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2 },
+        { 4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13 },
+    };
+    static const uint8_t P1[5][16] = {
+        { 11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8 },
+        { 7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12 },
+        { 11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5 },
+        { 11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12 },
+        { 9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6 },
+    };
+    static uint32_t (*F2[5])(uint32_t,uint32_t,uint32_t) = { j, i, h, g, f };
+    const uint32_t K2[5] = { 0x50a28be6, 0x5c4dd124, 0x6d703ef3, 0x7a6d76e9, 0x00000000 };
+    const uint8_t O2[5][16] = {
+        { 5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12 },
+        { 6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2 },
+        { 15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13 },
+        { 8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14 },
+        { 12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11 },
+    };
+    const uint8_t P2[5][16] = {
+        { 8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6 },
+        { 9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11 },
+        { 9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5 },
+        { 15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8 },
+        { 8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11 },
+    };
+    const uint32_t S[5] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 };
+    uint32_t s0 = S[0], s1 = S[1], s2 = S[2], s3 = S[3], s4 = S[4];
+    for (uint32_t j = 0; j < size/4; j += 16) {
+        uint32_t w[64];
+        for (int i = 0; i < 16; i++) {
+            w[i] = b2w32le(&message[4*(j+i)]);
+        }
+        for (int i = 16; i < 64; i++) {
+            w[i] = 0;
+        }
+        uint32_t u0, u1, u2, u3, u4;
+        {
+            uint32_t a = s0, b = s1, c = s2, d = s3, e = s4;
+            for (int i = 0; i < 5; i++) {
+                uint32_t (*f)(uint32_t,uint32_t,uint32_t) = F1[i];
+                uint32_t k = K1[i];
+                const uint8_t *o = O1[i];
+                const uint8_t *p = P1[i];
+                for (int j = 0; j < 16; j++) {
+                    a = rol(a + f(b, c, d) + w[o[j]] + k, p[j]) + e;
+                    c = rol(c, 10);
+                    uint32_t t = e; e = d; d = c; c = b; b = a; a = t;
+                }
+            }
+            u0 = a; u1 = b; u2 = c; u3 = d; u4 = e;
+        }
+        uint32_t v0, v1, v2, v3, v4;
+        {
+            uint32_t a = s0, b = s1, c = s2, d = s3, e = s4;
+            for (int i = 0; i < 5; i++) {
+                uint32_t (*f)(uint32_t,uint32_t,uint32_t) = F2[i];
+                uint32_t k = K2[i];
+                const uint8_t *o = O2[i];
+                const uint8_t *p = P2[i];
+                for (int j = 0; j < 16; j++) {
+                    a = rol(a + f(b, c, d) + w[o[j]] + k, p[j]) + e;
+                    c = rol(c, 10);
+                    uint32_t t = e; e = d; d = c; c = b; b = a; a = t;
+                }
+            }
+            v0 = a; v1 = b; v2 = c; v3 = d; v4 = e;
+        }
+        s0 += u1 + v2;
+        s1 += u2 + v3;
+        s2 += u3 + v4;
+        s3 += u4 + v0;
+        s4 += u0 + v1;
+        uint32_t t = s0; s0 = s1; s1 = s2; s2 = s3; s3 = s4; s4 = t;
+    }
+    w2b32le(s0, &output[0]);
+    w2b32le(s1, &output[4]);
+    w2b32le(s2, &output[8]);
+    w2b32le(s3, &output[12]);
+    w2b32le(s4, &output[16]);
+}
+
+static uint160_t ripemd160(const uint8_t *buffer, uint32_t size)
+{
+    uint8_t output[20];
+    ripemd160(buffer, size, false, output);
+    return uint160_t::from(output);
+}
+
 /* secp256k1 */
 
 const uint256_t _N[2] = {
