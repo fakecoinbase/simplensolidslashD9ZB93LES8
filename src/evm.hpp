7574063baa9ef4543f36728cb0853e2024be6334
Ricardo Toledo <rictoledo80@gmail.com>
2020-02-25 12:10:16 -0300

    Fixes range check for memory copying operations

--- src/evm.hpp
+++ src/evm.hpp
@@ -5335,10 +5335,10 @@ static bool _throws(vm_run)(Release release, Block &block, Storage &storage,
         case CALLDATACOPY: {
             uint256_t v1 = stack.pop(), v2 = stack.pop(), v3 = stack.pop();
             _handles0(memory_check)(v1, v3);
-            uint64_t offset1 = v1.cast64(), offset2 = v2.cast64(), size = v3.cast64();
+            uint64_t offset1 = v1.cast64(), size = v3.cast64();
+            uint64_t offset2 = v2 > call_size ? call_size : v2.cast64();
             _handles0(consume_gas)(gas, gas_memory(release, memory.size(), offset1 + size));
             _handles0(consume_gas)(gas, gas_copy(release, size));
-            if (v2 > call_size) offset2 = call_size;
             memory.burn(offset1, size, &call_data[offset2], _min(size, call_size - offset2));
             break;
         }
@@ -5346,10 +5346,10 @@ static bool _throws(vm_run)(Release release, Block &block, Storage &storage,
         case CODECOPY: {
             uint256_t v1 = stack.pop(), v2 = stack.pop(), v3 = stack.pop();
             _handles0(memory_check)(v1, v3);
-            uint64_t offset1 = v1.cast64(), offset2 = v2.cast64(), size = v3.cast64();
+            uint64_t offset1 = v1.cast64(), size = v3.cast64();
+            uint64_t offset2 = v2 > code_size ? code_size : v2.cast64();
             _handles0(consume_gas)(gas, gas_memory(release, memory.size(), offset1 + size));
             _handles0(consume_gas)(gas, gas_copy(release, size));
-            if (v2 > code_size) offset2 = code_size;
             memory.burn(offset1, size, &code[offset2], _min(size, code_size - offset2));
             break;
         }
@@ -5359,12 +5359,12 @@ static bool _throws(vm_run)(Release release, Block &block, Storage &storage,
             uint160_t address = (uint160_t)stack.pop();
             uint256_t v2 = stack.pop(), v3 = stack.pop(), v4 = stack.pop();
             _handles0(memory_check)(v2, v4);
-            uint64_t offset1 = v2.cast64(), offset2 = v3.cast64(), size = v4.cast64();
+            uint64_t offset1 = v2.cast64(), size = v4.cast64();
+            uint64_t offset2 = v3 > extcode_size ? extcode_size : v3.cast64();
             _handles0(consume_gas)(gas, gas_memory(release, memory.size(), offset1 + size));
             _handles0(consume_gas)(gas, gas_copy(release, size));
             uint64_t extcode_size;
             const uint8_t *extcode = storage.get_code(address, extcode_size);
-            if (v3 > extcode_size) offset2 = extcode_size;
             memory.burn(offset1, size, &extcode[offset2], _min(size, extcode_size - offset2));
             _delete(code);
             break;
@@ -5373,10 +5373,10 @@ static bool _throws(vm_run)(Release release, Block &block, Storage &storage,
         case RETURNDATACOPY: {
             uint256_t v1 = stack.pop(), v2 = stack.pop(), v3 = stack.pop();
             _handles0(memory_check)(v1, v3);
-            uint64_t offset1 = v1.cast64(), offset2 = v2.cast64(), size = v3.cast64();
+            uint64_t offset1 = v1.cast64(), size = v3.cast64();
+            uint64_t offset2 = v2 > return_size ? return_size : v2.cast64();
             _handles0(consume_gas)(gas, gas_memory(release, memory.size(), offset1 + size));
             _handles0(consume_gas)(gas, gas_copy(release, size));
-            if (v2 > return_size) offset2 = return_size;
             memory.burn(offset1, size, &return_data[offset2], _min(size, return_size - offset2));
             break;
         }
