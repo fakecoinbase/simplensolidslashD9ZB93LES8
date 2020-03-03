93f41d05dacf74be050b5a83913610347ec7ffd9
Ricardo Toledo <rictoledo80@gmail.com>
2020-03-03 15:31:45 -0300

    Adds a Dockerfile to setup the test environment

---
+++ docker/entrypoint.sh
@@ -0,0 +1,37 @@
+#/bin/sh
+
+sudo keosd &
+nodeos -e -p eosio \
+	--plugin eosio::producer_plugin \
+	--plugin eosio::chain_api_plugin \
+	--plugin eosio::http_plugin \
+	--plugin eosio::history_plugin \
+	--plugin eosio::history_api_plugin \
+	--filter-on="*" \
+	--access-control-allow-origin='*' \
+	--contracts-console \
+	--http-validate-host=false \
+	--verbose-http-errors >> nodeos.log 2>&1 &
+sleep 1
+cleos wallet create --to-console && \
+echo '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3' | cleos wallet import
+echo '5J2CnKzrj7XU2nHdzqrJ5yZou94tUUk3HB3TiArDCFgSAQmA6qc' | cleos wallet import
+cleos create account eosio alice EOS72H7tRCDjsPzkdYjmdSnENCrA25D2Q1ZopCyYojBd9cjVa2yqs
+cleos create account eosio bob EOS72H7tRCDjsPzkdYjmdSnENCrA25D2Q1ZopCyYojBd9cjVa2yqs
+cleos create account eosio eosio.token EOS72H7tRCDjsPzkdYjmdSnENCrA25D2Q1ZopCyYojBd9cjVa2yqs
+cd contracts/evm/
+cd eosio.contracts/contracts/eosio.token/
+cleos set contract eosio.token . --abi eosio.token.abi -p eosio.token@active
+cleos push action eosio.token create '["eosio", "1000000000.0000 SYS"]' -p eosio.token@active
+cleos push action eosio.token issue '["eosio", "10000.0000 SYS", "memo"]' -p eosio@active
+cleos push action eosio.token transfer '["eosio", "alice", "1000.0000 SYS", "memo"]' -p eosio@active
+cleos push action eosio.token transfer '["eosio", "bob", "1000.0000 SYS", "memo"]' -p eosio@active
+cd ../../..
+cleos create account eosio evm EOS72H7tRCDjsPzkdYjmdSnENCrA25D2Q1ZopCyYojBd9cjVa2yqs -p eosio@active
+cleos set account permission evm active --add-code
+cleos set contract evm . -p evm@active
+cleos push action evm create '["alice", ""]' -p alice@active
+cleos push action evm create '["bob", ""]' -p bob@active
+cleos push action eosio.token transfer '["alice", "evm", "1000.0000 SYS", "memo"]' -p alice@active
+cleos push action eosio.token transfer '["bob", "evm", "1000.0000 SYS", "memo"]' -p bob@active
+
---
+++ docker/start.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+SCRIPT=`realpath $0`
+FOLDER=`dirname $SCRIPT`
+
+docker build -t $USER/eosio --build-arg USER=$USER $FOLDER
+docker run -it --rm --name eosio -v $FOLDER/../:$HOME/eosio/ -w $HOME/eosio/ -e TERM=$TERM $USER/eosio bash
