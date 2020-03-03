7453c0cb7c0d1562e613f13632b44f43fee79a27
Ricardo Toledo <rictoledo80@gmail.com>
2020-03-03 19:46:05 -0300

    Fixes paths in the docker configuration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -21,15 +21,17 @@ echo '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3' | cleos wallet import
 echo '5J2CnKzrj7XU2nHdzqrJ5yZou94tUUk3HB3TiArDCFgSAQmA6qc' | cleos wallet import
 
 cleos create account eosio eosio.token EOS72H7tRCDjsPzkdYjmdSnENCrA25D2Q1ZopCyYojBd9cjVa2yqs
-cd contracts/evm/eosio.contracts/contracts/eosio.token/
+cd support/eosio.contracts/contracts/eosio.token/
+eosio-cpp -I include -o eosio.token.wasm src/eosio.token.cpp --abigen
 cleos set contract eosio.token . --abi eosio.token.abi -p eosio.token@active
-cd ../../..
+cd ../../../..
 cleos push action eosio.token create '["eosio", "1000000000.0000 SYS"]' -p eosio.token@active
 cleos push action eosio.token issue '["eosio", "10000.0000 SYS", "memo"]' -p eosio@active
 
 cleos create account eosio evm EOS72H7tRCDjsPzkdYjmdSnENCrA25D2Q1ZopCyYojBd9cjVa2yqs -p eosio@active
 cleos set account permission evm active --add-code
 cd contracts/evm
+make
 cleos set contract evm . -p evm@active
 cd ../..
 
