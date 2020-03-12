5f5eb886fc2bd6f062c4deb6b6fd8b71c3496142
simplensolid <rictoledo80@gmail.com>
2020-03-12 08:38:10 +0000

    Reviews quick docker start script

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,4 @@ SCRIPT=`realpath -s $0`
 FOLDER=`dirname $SCRIPT`
 
 docker build -t $USER/eosio --build-arg USER=$USER $FOLDER
-#docker run -it --rm --name eosio --hostname eosio -v $FOLDER/../:$HOME/eosio/ -w $HOME/eosio/ -e TERM=$TERM $USER/eosio bash
-docker run -it --rm --hostname eosio -v $FOLDER/../:$HOME/eosio/ -w $HOME/eosio/ -e TERM=$TERM $USER/eosio bash
-
+docker run -it --rm --name eosio --hostname eosio -v $FOLDER/../:$HOME/eosio/ -w $HOME/eosio/ -e TERM=$TERM $USER/eosio bash
